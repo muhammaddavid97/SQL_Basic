eae053c7ea9d18a0ec2cb4e01e2ed66e646f5c88
muhammaddavid97 <68804931+muhammaddavid97@users.noreply.github.com>
2022-07-11 12:12:34 +0700

    first commit

---
+++ insert_query.sql
@@ -0,0 +1,9 @@
+/*
+ INSERT [INTO] nama_table SET nama_kolom1 = nilai1, nama_kolom2 = nilai2, dst.. 
+ nama_table => nama table yang akan diisi datanya
+ nama_kolom1 => nama kolom pertama yang akan diinput data
+ nilai_1 => data pertama pada kolom pertama 
+ nama_kolom2 => nama kolom kedua yang akan diinput data
+ nilai_2 => data kedua pada kolom kedua.
+*/
+
