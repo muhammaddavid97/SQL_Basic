0e4c51e13b3aecbd0e35442de54674f10650e8d8
muhammaddavid97 <68804931+muhammaddavid97@users.noreply.github.com>
2022-07-13 10:02:22 +0700

    second commit

--- Select_query.sql
+++ Select_query.sql
@@ -88,3 +88,36 @@ SELECT * FROM populasi ORDER BY kec OFFSET 3 ROWS
 -- menampilkan 3 data pertama dan 2 data berikutnya diurutkan berdasarkan luas, gunakan kalusa offset dan fetch 
 
 SELECT * FROM populasi ORDER BY luas OFFSET 3 FETCH NEXT 3 ROWS ONLY
+
+/*
+   SELECT WHERE adalah perintah yang digunakan untuk melakukan filterisasi atau membatasi data yang ditampilkan
+    berdasarkan kondisi tertentu. format query sebagai berikut
+    
+    SELECT nama_kolom FROM nama_table WHERE kondisi1 [AND | OR] kondisi2
+*/
+
+-- menampilkan data pada kolom kota dan kelurahan dimana luasnya adalah 265
+SELECT kota, kel AS 'kelurahan' FROM populasi WHERE luas = 265
+
+-- menampilkan seluruh data pada table populasi yang memiliki jumlah kecamatan lebih dari 20
+SELECT * FROM populasi WHERE kec > 20
+
+-- menampilkan data table populasi-- Tampilkan kota dengan jumlah kecamatan lebih dari 20 atau luas kota kurang dari 300
+SELECT kota FROM populasi WHERE kec > 20 OR luas < 300 yang memiliki kota tanpa selain jakarta dan bandung
+SELECT * from populasi WHERE kota != 'Jakarta' AND kota != 'Bandung'
+
+-- menampilkan data populasi dimana nama kotanya jakarta atau jumlah kel kurang dari 100 
+SELECT * FROM populasi WHERE kota = 'Jakarta' OR kel < 100
+
+-- Tampilkan kota dengan jumlah kecamatan lebih dari 20 dengan luas kota kurang dari 300 km2
+SELECT kota FROM populasi WHERE kec > 20 AND luas < 300
+
+-- Tampilkan kota dengan jumlah penduduk antara 1.000.000 hingga 2.000.000.
+SELECT kota FROM populasi WHERE penduduk >= 1000000 AND penduduk >= 2000000
+
+/*
+Tampilkan kota dengan jumlah kecamatan lebih besar dari 20, atau memiliki luas kurang dari
+300 km2, dimana setiap kota itu harus memiliki jumlah penduduk kurang dari 2.000.000
+*/
+SELECT kota FROM populasi WHERE (kec > 20 OR luas < 300) AND penduduk < 2000000
+
