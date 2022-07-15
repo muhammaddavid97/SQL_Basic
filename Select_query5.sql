8f5867c1337202aacb147f0afb37157770493011
muhammaddavid97 <68804931+muhammaddavid97@users.noreply.github.com>
2022-07-15 11:22:50 +0700

    second_commit

--- Select_query5.sql
+++ Select_query5.sql
@@ -26,5 +26,28 @@ GROUP BY
 jurusan
 HAVING jumlah_jurusan > 1
 
+-- Tampilkan data nilai uan minimum, nilai uan maksimum, serta nilai uan rata-rata untuk setiap jurusan yang dipilih mahasiswa
+SELECT jurusan, nilai_uan, MAX(nilai_uan), MIN(nilai_uan), AVG(nilai_uan)
+FROM 
+mahasiswa
+GROUP BY 
+jurusan
 
+
+
+CREATE TABLE universitas(
+jurusan varchar(25) not null,
+tgl_beridiri date not null,
+nama_dekan varchar(50) not null,
+jumlah_mhs int not null,
+akreditas char(1) null
+)
+
+INSERT INTO universitas VALUES
+('Kimia', '1987-07-12', 'Prof. Mulyono, M.Sc.', 662, 'B'),
+('Ilmu Komputer', '2003-02-23', 'Dr. Syahrial, M.Kom.', 412, 'A'),
+('Akuntansi', '1985-03-19', 'Maya Fitrianti, M.M.', 895, 'B'),
+('Farmasi', '1997-05-30', 'Prof. Silvia Nst, M.Farm.', 312, 'C'),
+('Fisika', '1989-12-10', 'Dr. Umar Agustinus, M.Sc.', 275, 'A'),
+('Hukum', '1983-08-08', 'Prof. Gunarto, M.H.', 754, 'B');
 
