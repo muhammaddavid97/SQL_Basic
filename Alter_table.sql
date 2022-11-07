b56c9fa2250a4138da16659a0775fce2f08dbb07
muhammaddavid97 <68804931+muhammaddavid97@users.noreply.github.com>
2022-11-07 20:00:55 +0700

    first commit

--- Alter_table.sql
+++ Alter_table.sql
@@ -1,40 +1,45 @@
 /*
-	ALTER TABLE perintah yang digunakan untuk mengubah struktur table baik itu menambah kolom baru,
-    mengubah nama table, mengubah tipe data kolom dll
-    
-    format dasar bentuk dari query ALTER
-    
-    ALTER TABLE nama_table 
+   ALTER TABLE => Berfungsi untuk mengubah struktur table baik itu mengganti nama table, nama kolom, tipe data kolom, menambahkan kolom baru
+   menghapus kolom yang sudah ada hingga mengganti storage engine dari sebuah table.
+   
+   syntax berikut : 
+   	ALTER TABLE nama_table [alter_specification [, alter_specification] ...]
 */
 
--- sp_rename digunakan untuk mengubah nama table 
-
-CREATE TABLE person(
-nama varchar(30) not null,
-age int not null
-) 
-
-sp_rename 'person', 'person2'
-
-
 /*
-	ALTER TABLE nama_table ALTER COLUMN column_name new_type_data(size) digunakan untuk mengubah tipe data 
-    dan atribut kolom
+   ALTER TABLE ... RENAME => berfungsi untuk mengubah nama table
+   
+   syntax berikut : 
+   	ALTER TABLE nama_table_lama RENAME [TO | AS] nama_table_baru
 */
 
-ALTER TABLE Person ALTER COLUMN address VARCHAR(15)
+CREATE TABLE contoh_alter1(
+	a int AUTO_INCREMENT PRIMARY KEY,
+    b VARCHAR(20) NOT NULL
+);
+
+ALTER TABLE contoh_alter1 RENAME TO contoh_alter01;
+
+DESC contoh_alter01
 
 /*
-  ALTER TABLE ADD COLUMN digunakan untuk menambahkan kolom baru ke dalam table format dasar bentuk query ini 
+	ALTER TABLE MODIFY => Berfungsi untuk mengubah tipe data dan atribut dari sebuah kolom
+    
+    syntax berikut : 
+    	ALTER TABLE nama_table MODIFY nama_kolom tipe_data_kolom [attribut_kolom];
+*/
+
+ALTER TABLE contoh_alter01 MODIFY a tinyint AUTO_INCREMENT;
+
+DESC contoh_alter01;
+
+/*
+  ALTER TABLE CHANGE => berfungsi untuk mengubah nama kolom sekaligus tipe datanya
   
-  ALTER TABLE name_table ADD nama_colum type_data attribute_column
+  syntax berikut : 
+  	ALTER TABLE nama_table CHANGE nama_kolom nama_kolom_baru tipe_kolom [attribut_kolom];
 */
 
-CREATE TABLE dosen (
-nik char(10) not null,
-name varchar(30) not null
-) 
+ALTER TABLE contoh_copy1 CHANGE c d timestamp;
 
--- menambahkan kolom baru ke dalam table dosen
-ALTER TABLE dosen ADD address varchar(30) Not Null
-
+DESC contoh_copy1;
