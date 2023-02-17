a494c4fd3570b5cc0272209d9cf5e5ebf249232d
muhammaddavid97 <68804931+muhammaddavid97@users.noreply.github.com>
2023-02-17 11:45:04 +0700

    Update Regex.sql

--- Regex.sql
+++ Regex.sql
@@ -43,6 +43,40 @@
     
     -- menampilkan data provinsi dimana nama ibukota setidaknya memiliki satu huruf B diikuti dengan huruf antara A sampai E
     SELECT * FROM Provinsi WHERE Ibukota RLIKE 'b[A-E]'
+      
+*/
+
+/*
+  Negasi Kumpulan Karakter
+    Negasi kumpulan karakter berfungsi untuk membalikan logika dari sekumpulan karakter, dengan menyertakan simbol '^'
+    
+    -- menampilkan data provinsi dimana nama ibukota setidak memiliki satu huru B yg diikuti selain huruf vokal
+    SELECT * FROM Provinsi WHERE Ibukota RLIKE 'B[^aiueo]'
+    
+    -- menampilkan data provinsi dimana nama ibukota setidaknya memiliki satu huruf J yg diikuti dengan huruf vokal
+    SELECT * FROM Provinsi WHERE Ibukota RLIKE '^J[aiueo]'  
+*/
+
+/*
+  Pola lpgika OR
+    Pola logika OR digunakan untuk membuat kondisi logika 'or' yang berfungsi sebagai alternatif pola
+    Untuk membuat logika OR digunakan simbol "|"
+    
+    -- menampilkan data provinsi dimana nama ibukota berisi string ta, ba atau pa
+    SELECT * FROM Provinsi WHERE Ibukota RLIKE 'ta|ba|pa'
+    
+*/
+
+/*
+  Membatasi jumlah karakter
+    Regex juga dapat membatasi jumlah karakter tertentu, misalnya digit angka harus ditulis sebanyak 8-12 kali untuk proses validasi
+    Karakter yang digunakan untuk membatasi jumlah karakter adalah simbol { dan }
+    
+    -- Menampilkan daftar provinsi dimana nama ibukota setidaknya memiliki 2 buah huruf s
+    SELECT * FROM Provinsi WHERE Ibukota RGLIKE 'S{2}'
+    
+    -- menampilkan string dimana karakternya berisi aaa99
+    SELECT 'aaa99' RGLIKE 'a{3}9{2}'
     
     
 */
