dbf857cbc567d90a9d6a294b1aec1842d856f9e3
muhammaddavid97 <68804931+muhammaddavid97@users.noreply.github.com>
2023-10-26 17:17:25 +0700

    Update Select_query3.sql

--- Select_query3.sql
+++ Select_query3.sql
@@ -1,34 +1,53 @@
-/*
-	SELECT LIKE digunakan untuk melakukan proses pencarian berdasarkan pola pattern. format penulisan query
-    
-    SELECT nama_kolom FROM nama_table WHERE nama_kolom LIKE pola
-    
-    menyediakan 2 karakter khusus untuk pola pencarian
-    • _ : karakter ganti yang cocok untuk satu karakter apa saja.
-	• % : karakter ganti yang cocok untuk karakter apa saja dengan panjang karakter tidak
-		  terbatas, termasuk tidak ada karakter.
-*/
+-- SELECT nama_kolom FROM nama_table WHERE nama_kolom IN (nilai_kolom) 
+-- SELECT nama_kolom FROM nama_table WHERE nama_kolom NOT IN (nilai_kolom) 
+-- query ini berfungsi untuk menampilkan data berdasarkan kondisi nilai himpunan 
+-- IN => operator yang berfungsi untuk menampilkan data berdasarkan himpunannya 
+-- NOT IN => kebalikan dari operator IN 
 
--- menampilkan tabel populasi dimana nama kota diawali dengan huruf B:
-SELECT * FROM populasi WHERE kota LIKE 'B%'
+-- menampilkan tabel populasi dimana kolom kota ada di dalam himpunan Jakarta, Bandung dan Medan:
+SELECT * FROM Populasi WHERE kota IN ('Bandung', 'Jakarta', 'Medan')
 
--- menampilkan tabel daftar_provinsi yang nama provinsinya diawali string Sumatera:
-SELECT * FROM daftar_provinsi WHERE prov LIKE 'Sumatera%'
+-- menampilkan tabel populasi dimana kolom kota selain yang ada di dalam himpunan Jakarta, Bandung dan Medan
+SELECT * FROM Populasi WHERE kota NOT IN ('Jakarta', 'Bandung', 'Medan')
 
-/*
-	SELECT UNION berfungsi untuk menggabungkan dua atau lebih query SELECT. dimana jumlah kolom pada query SELECT pertama harus
-    sama dengan jumlah kolom pada query select kedua jika tidak akan menampilkan error. format query sebagai berikut
-    
-    SELECT nama_kolom1, [nama_kolom2] FROM nama_table1
-    UNION
-    SELECT nama_kolom3, [nama_kolom4] FROM nama_table2
-    
-    secara default query UNION hanya akan menampilkan satu data yang nilainya sama diantara ke dua table
-*/
+-- menampilkan table populasi dimana kolom kota merupakan ibukota provinsi 
+SELECT * FROM Populasi WHERE kota IN (SELECT ibukota FROM daftar_provinsi)
 
--- menggabungkan kolom kota dari tabel populasi dengan kolom ibukota dari tabel daftar_provinsi:
-SELECT kota AS 'Daftar kota' FROM populasi UNION SELECT ibukota FROM daftar_provinsi
+-- SELECT nama_kolom FROM nama_table WHERE nama_kolom =  [ANY() | SOME() | ALL()]
+-- query ini memiliki fungsi yang sama seperti query IN tetapi sedikit lebih kompleks 
+-- query ini biasanya dipakai pada saat menggunakan subquery (query bersarang)
+-- ketiga query ini dapat menggunakan berbagai macam operator perbandingan 
 
--- menampilkan kolom kota pada table populasi dan kolom ibukota pada table daftar_provinsi secara bersamaan 
--- kemudian urutkan data secara descending
-SELECT kota FROM populasi UNION SELECT ibukota FROM daftar_provinsi ORDER BY kota DESC
+-- Tampilkan seluruh data yang ada di tabel populasi, dimana nama kotanya sama dengan salah satu himpunan nama ibukota yang ada di dalam tabel daftar_provinsi
+SELECT * FROM Populasi WHERE kota = ANY(SELECT ibukota FROM daftar_provinsi)
+
+-- SELECT EXISTS (subquery)
+-- SELECT nama_kolom FROM nama_table WHERE EXITST(subquery)
+-- query ini digunakan untuk memeriksa apakah subquery ada atau tidak hasilnya true jika terdapat minimal satu baris atau
+-- false jika tidak ditemukan baris sama sekali. nilai  true akan dikonversi menjadi 1 dan false akan dikonversi menjadi 0
+-- untuk membalikan logika EXIST kita bisa menggunakan query NOT EXISTS 
+
+SELECT EXISTS (SELECT kota FROM Populasi WHERE kota = 'Bandung') -- true 
+SELECT EXISTS (SELECT kota FROM Populasi WHERE kota = 'Kupang') -- false 
+
+-- menampilkan seluruh baris pada table Populasi berdasarkan kondisi perintah EXISTS 
+SELECT * FROM Populasi WHERE EXISTS(SELECT ibukota FROM daftar_provinsi WHERE ibukota = 'Bandung')
+
+-- SELECT nama_kolom FROM nama_table WHERE nama_kolom BETWEEN nilai_kolom1 AND nilai_kolom2 
+-- query ini digunakan untuk menampilkan data berdasarkan kondisi dengan range tertentu 
+-- NOT BETWEEN query ini digunakan untuk membalikan logika BETWEEN 
+
+-- menampilkan isi tabel populasi dimana nilai kolom kecamatan antara 20 dan 30
+SELECT * FROM Populasi WHERE kec BETWEEN 20 AND 30
+
+--  menampilkan isi tabel populasi dimana nilai kolom penduduk antara 1.000.000 dan 2.000.000:
+SELECT * FROM Populasi WHERE penduduk BETWEEN 1000000 AND 2000000
+
+-- menampilkan isi tabel populasi dimana nilai kolom penduduk bukan diantara 1.000.000 dan 2.000.000
+SELECT * FROM Populasi WHERE penduduk NOT BETWEEN 1000000 AND 2000000
+
+-- menampilkan tabel daftar_provinsi dimana tanggal diresmikan antara 09-08-1957 hingga 01-01-2017:
+SELECT * FROM daftar_provinsi WHERE tanggal_diresmikan BETWEEN '09-08-1957' AND '01-01-2017' 
+
+-- Tampilkan isi tabel daftar_provinsi dimana tanggal diresmikan antara 09-08-1957 hingga 01-01-2017, serta tidak termasuk salah satu dari kota ‘Denpasar’, ‘Jakarta’, dan ‘Medan
+SELECT * FROM daftar_provinsi WHERE (tanggal_diresmikan BETWEEN '09-08-1957' AND '01-01-2017') AND ibukota not IN ('Denpasar', 'Jakarta', 'Medan')
